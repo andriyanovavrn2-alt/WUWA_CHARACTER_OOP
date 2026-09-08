7cfb8f64552a7a2f1b8dc3ff184db54309fa34df
Lena <elena.andrian0v@yandex.ru>
2026-09-08 22:43:33 +0300

    08_09_26

--- WUWA_CHARACTER_OOP/Program.cs
+++ WUWA_CHARACTER_OOP/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -15,15 +16,27 @@ namespace _30_08_26_stepic
 {
     internal class Program
     {
+        public static void PrintAllChar(List<Character> char_list)
+        {
+            Console.WriteLine("All character: ");
+            for (int i = 0; i < char_list.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}) {char_list[i].Name}");
+            }
+        }
+
+
         public static void Main()
         {
+            bool flag = true;
             List<Character> char_list = new List<Character>();
+
             const double hp = 10_000;
             const double atk = 500;
             const double def = 1_500;
             const double crit_rate = 5;
             const double crit_dmg = 50;
-            while (true)
+            while (flag)
             {
                 Console.WriteLine("-----Main Menu-----");
                 Console.WriteLine("Hi! Chose the number:");
@@ -44,30 +57,52 @@ namespace _30_08_26_stepic
                         if (choice_weapon == "Sword" || choice_weapon == "sword")
                         {
                             Character sword_char = new SwordChar(name_char, choice_weapon, elem_char, hp, atk, def, crit_rate, crit_dmg, role_char, 25);
+                            char_list.Add(sword_char);
                         }
                         else if (choice_weapon == "Claymore" || choice_weapon == "claymore")
                         {
-
+                            Character claymore_char = new ClaymoreChar(name_char, choice_weapon, elem_char, hp, atk, def, crit_rate, crit_dmg, role_char, 15);
+                            char_list.Add(claymore_char);
                         }
-
-
-                        Console.ReadLine();
+                        Console.WriteLine("Your character add in list!");
+                        Console.WriteLine("\n\n");
                         break;
                     case "2":
-                        Console.WriteLine("All character");
-                        foreach (var i in char_list)
+                        if (char_list.Count == 0)
                         {
-                            Console.WriteLine(i);
+                            Console.WriteLine("The list is empty.");
                         }
+                        else
+                            PrintAllChar(char_list);
+                        Console.WriteLine("\n\n");
                         break;
                     case "3":
-                        Console.WriteLine("Ditales of character");
+                        if (char_list.Count == 0)
+                        {
+                            Console.WriteLine("The list is empty.");
+                        }
+                        else
+                            PrintAllChar(char_list);
+
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Chose the character (enter the character number): ");
+                        int choice_num = Convert.ToInt32(Console.ReadLine());
+                        if (choice_num > char_list.Count || choice_num <= 0)
+                        {
+                            Console.WriteLine("Uncorrect character number!");
+                        }
+                        else
+                        {
+                            char_list[choice_num - 1].PrintStats();
+                            Console.WriteLine("Ditales of character: ");
+                        }
                         break;
                     case "4":
                         Console.WriteLine("Enter character for delete");
                         break;
                     case "5":
                         Console.WriteLine("Bye");
+                        flag = false;
                         break;
                     default:
                         Console.WriteLine("Uncorrect enter!");
@@ -75,5 +110,6 @@ namespace _30_08_26_stepic
                 }
             }
         }
+
     }
 }
